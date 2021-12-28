7acbc6d82a5ac6c2e45f156a8b8c5bdc8dda6070
Omri Avisar <omric500@gmail.com>
2021-12-28 15:05:59 +0200

    Add shell for basic protocol analyzers

--- Pcap2Network/Analyzer/layer2.py
+++ Pcap2Network/Analyzer/layer2.py
@@ -1,2 +1,29 @@
+PROTOCOLS = {}
+
+
+def protocol_analyzer(protocol_name):
+    def decorator(f):
+        PROTOCOLS[protocol_name] = f
+        return f
+
+    return decorator
+
+
+def analyze_metadata(db, metadata):
+    pass
+
+
 def layer2_analyze_packet(db, packet, pcap_metadata):
-    return None
+    analyze_metadata(db, pcap_metadata)
+    for protocol in PROTOCOLS:
+        if protocol in packet:
+            PROTOCOLS[protocol](db, packet, pcap_metadata)
+
+    return
+
+
+@protocol_analyzer('Ether')
+def ether_analyzer(db, packet, pcap_metadata):
+    dst = packet['Ether'].dst
+    src = packet['Ether'].src
+    return
--- Pcap2Network/Analyzer/layer4.py
+++ Pcap2Network/Analyzer/layer4.py
@@ -1,2 +1,41 @@
+PROTOCOLS = {}
+
+
+def protocol_analyzer(protocol_name):
+    def decorator(f):
+        PROTOCOLS[protocol_name] = f
+        return f
+
+    return decorator
+
+
+def analyze_metadata(db, metadata):
+    pass
+
+
 def layer4_analyze_packet(db, packet, pcap_metadata):
-    return None
+    analyze_metadata(db, pcap_metadata)
+    for protocol in PROTOCOLS:
+        if protocol in packet:
+            PROTOCOLS[protocol](db, packet, pcap_metadata)
+
+    return
+
+
+@protocol_analyzer('TCP')
+def ether_analyzer(db, packet, pcap_metadata):
+    dport = packet['TCP'].dport
+    sport = packet['TCP'].sport
+    return
+
+
+@protocol_analyzer('UDP')
+def ether_analyzer(db, packet, pcap_metadata):
+    dport = packet['UDP'].dport
+    sport = packet['UDP'].sport
+    return
+
+
+@protocol_analyzer('ICMP')
+def ether_analyzer(db, packet, pcap_metadata):
+    return
