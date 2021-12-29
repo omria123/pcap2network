61fe6f18e62effa10d78dfd932d4ec3cae02b135
Omri Avisar <omric500@gmail.com>
2021-12-29 11:29:15 +0200

    Designing merging operations

--- Pcap2Network/Machine/machine.py
+++ Pcap2Network/Machine/machine.py
@@ -25,15 +25,18 @@ class Machine(object):
     @classmethod
     def merge_machines(cls, machine1, machine2):
         interfaces = machine1.interfaces
-        macs = [i.mac for i in interfaces]
+        macs = {i.mac: i for i in interfaces}
         for interface in machine2.interfaces:
             if interface.mac in macs:
+                other_interface = macs[interface.mac]
+                interfaces.remove(other_interface)
+                interfaces.append(interface.merge_interfaces(interface, macs[interface.mac]))
 
-
-
-
-        cls(machine1.os or machine2.os)
         if machine1.os is not None and machine2.os is not None and machine1.os != machine2.os:
             pass
             # TODO: Add logging alert that a conflict just happened. Printing an identifier and the relevant OSes.
 
+        return cls(machine1.os or machine2.os, interfaces)
+
+#### Merges Diagram #####
+# m1, m2 Merge -> m1.interfaces, m2.interfaces merge -> (if same interface) i1.network, i2.network merge -> all interfaces in both lans should be in a new lan
