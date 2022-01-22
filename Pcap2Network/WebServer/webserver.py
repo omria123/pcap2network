c17eba169c0fafacb3e435be4cc6c2de11b52e8c
Omri Avisar <omric500@gmail.com>
2022-01-22 15:57:25 +0200

    Create webserver skeleton

--- Pcap2Network/WebServer/webserver.py
+++ Pcap2Network/WebServer/webserver.py
@@ -1,8 +1,29 @@
+import typing
+
+if typing.TYPE_CHECKING:
+    from ..Database import Database
+    from typing import Union
+
+
 class WebServer:
     DEFAULT_PORT = 8080
 
-    def __init__(self, port=DEFAULT_PORT):
-        self.port = port
+    def __init__(self, port=DEFAULT_PORT, db=None):
+        # type: (int, Database) -> None
+        self.port = port  # type: int
+        self.db = db  # type: Union[Database, None]
+
+    def upload_pcap(self):
+        """
+        Gets a POST request and receive the pcap to analyze it.
+        """
+
+    def run_query(self):
+        """
+        Run query to the DB and return the results.
+        """
 
     def run(self):
-        pass
+        """
+        Start the server with all of it's functions
+        """
